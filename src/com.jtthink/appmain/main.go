ebee7dee5522289266f575a2afc26cc4a3f1852f
shijun <shijun@qq.com>
2019-12-24 23:00:06 +0800

    用切片返回查询结果

--- src/com.jtthink/appmain/main.go
+++ src/com.jtthink/appmain/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "com.jtthink/Servicesb"
+	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -10,41 +11,36 @@ type all interface{} //声明一个空接口，
 
 func main()  {
 
-	var i all=123 //空接口可以赋值为任意类型
-	fmt.Println(i)
-
-	var ii interface{} = "zhangsan" //匿名空接口
-	fmt.Println(ii)
-
-	list:=make([]interface{},2) //声明一个空类型，类型是空接口
-	list[0]=1
-	list[1]="lisi"
-	fmt.Println(list)
-
-//// // 链接mysql数据库
-//	db,_ := sql.Open("mysql", "root:123456@tcp(localhost:3306)/test?charset=utf8mb4")
-//
-//	rows,_:=db.Query("select user_id,user_name from user")
-//	fmt.Println(rows.Columns())
-
-	//// var newsModel models.NewsModel=models.NewsModel{} // 两种变量申明方式
-	//newsModel:=models.NewsModel{} // 两种变量申明方式
-	//for rows.Next(){
-	//	//var uid int
-	//	//var uname string
-	//	//rows.Scan(&uid,&uname) //赋值查询结果到变量
-	//	//fmt.Println(uid,uname)
+	//var i all=123 //空接口可以赋值为任意类型
+	//fmt.Println(i)
 	//
-	//	rows.Scan(&newsModel.Id,&newsModel.Title) //查询结果赋值到结构体
-	//	fmt.Println(newsModel)
-	//}
+	//var ii interface{} = "zhangsan" //匿名空接口
+	//fmt.Println(ii)
+	//
+	//list:=make([]interface{},2) //声明一个空类型，类型是空接口
+	//list[0]=1
+	//list[1]="lisi"
+	//fmt.Println(list)
 
-	//newsModels:=[]models.NewsModel{} //声明切片，返回一个新闻列表
-	//for rows.Next(){
-	//	tmp:=models.NewsModel{}
-	//	rows.Scan(&tmp.Id,&tmp.Title) //查询结果赋值到结构体
-	//	newsModels=append(newsModels,tmp)
-	//}
-	//fmt.Println(newsModels)
+// // 链接mysql数据库
+	db,_ := sql.Open("mysql", "root:123456@tcp(localhost:3306)/test?charset=utf8mb4")
+
+	rows,_:=db.Query("select user_id,user_name from user")
+	fmt.Println(rows.Columns())
+
+	allRows:=make([]interface{},0)
+	for rows.Next(){
+		var uid int
+		var uname string
+		rows.Scan(&uid,&uname)
+		//oneRow:=make([]interface{},2) //第一种方式
+		//oneRow[0]=uid
+		//oneRow[1]=uname
+		//allRows=append(allRows,oneRow)
+
+		allRows=append(allRows,[]interface{}{uid,uname}) //第二种简化方式
+
+	}
+	fmt.Println(allRows)
 
 }
