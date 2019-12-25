39a73c48d6ec42bc22b1aaf9d1271f8fe63f5f59
shijun <shijun@qq.com>
2019-12-25 23:16:56 +0800

    go 带缓冲的channel

--- src/com.jtthink/appmain/main.go
+++ src/com.jtthink/appmain/main.go
@@ -15,14 +15,11 @@ func sum(max int,c chan int)  {
 }
 func main()  {
 
-	c:=make(chan int) //声明channel
-	go sum(100,c) // 通过channel实现主线程和协程交互
+	c:=make(chan int,2) //带缓冲的channel。声明channel,第二个参数是容量。带有容量=2的 channel (好比队列)，只有当队列塞满时发送者会阻塞，队列空时接受者会阻塞
+	sum(100,c) //
 	res:=<-c //数据接收者
 	fmt.Println(res)
-	//上面的代码中
-	//    对于发送者：如果没有接收者读取 channel （<- channel），则发送者 (channel <-) 会一直阻塞。
-	//   同样对于接收者： 接收操作是阻塞的直到发送者发送数据
-	//   根据上面的特性，就能实现当goroutine执行完成后  得到数据
+
 
 
 
