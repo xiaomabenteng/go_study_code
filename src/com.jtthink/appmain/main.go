014126530596e55e60c61b434f65242a2c9e6de2
shijun <shijun@qq.com>
2019-12-25 23:33:50 +0800

    go 协程 demo

--- src/com.jtthink/appmain/main.go
+++ src/com.jtthink/appmain/main.go
@@ -4,21 +4,34 @@ import (
 	_ "com.jtthink/Servicesb"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
-func sum(max int,c chan int)  {
+func sum(min,max int,c chan int)  {
 	res:=0
-	for i:=0;i<=max;i++ {
+	for i:=min;i<=max;i++ {
 		res=res+i
 	}
-	c<-res //数据写入者
+	c<-res
 }
 func main()  {
 
-	c:=make(chan int,2) //带缓冲的channel。声明channel,第二个参数是容量。带有容量=2的 channel (好比队列)，只有当队列塞满时发送者会阻塞，队列空时接受者会阻塞
-	sum(100,c) //
-	res:=<-c //数据接收者
-	fmt.Println(res)
+	start:=time.Now()
+	num:=100000000
+	c:=make(chan int,2)
+
+
+	////sum(0,num,c) 同步执行
+	//go sum(0,num,c) 协程执行，两者基本相同
+	//res:=<-c //数据接收者
+
+	go sum(0,num/2,c) //将数据分成两份，分别放入两个协程运行。总体运行时间减少为一半
+	go sum(num/2+1,num,c)
+
+	res1,res2:=<-c,<-c
+
+	end:=time.Now()
+	fmt.Println(end.Sub(start),res1+res2)
 
 
 
