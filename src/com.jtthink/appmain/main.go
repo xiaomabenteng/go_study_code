0e352c9b09aea365ffbb1b9202b3a36bf690afe0
shijun <shijun@qq.com>
2019-12-26 23:41:38 +0800

    使用协程读取网页，并写入文件

--- src/com.jtthink/appmain/main.go
+++ src/com.jtthink/appmain/main.go
@@ -14,14 +14,24 @@ func main()  {
 
 url:="https://news.cnblogs.com/n/page/%d/"
 
+c:=make(chan map[int][]byte)
 for i:=1;i<=3;i++{
-	url:=fmt.Sprintf(url,i) //格式化字符串
-	res,_:=http.Get(url) //http请求
-	cnt,_:=ioutil.ReadAll(res.Body) //获取body
-	ioutil.WriteFile(fmt.Sprintf("./files/%d",i),cnt,666) //输出文件
+
+	go func(index int) {
+		url:=fmt.Sprintf(url,index) //格式化字符串
+		res,_:=http.Get(url) //http请求
+		cnt,_:=ioutil.ReadAll(res.Body) //获取body
+		c<-map[int][]byte{index:cnt} //将序号，和网页内容写入channel
+		if index==3{
+			close(c)
+		}
+	}(i)
 }
-
-
+	for getcnt:=range c {
+		for i,v:=range getcnt{
+			ioutil.WriteFile(fmt.Sprintf("./files/%d",i),v,666) //输出文件
+		}
+	}
 
 
 
