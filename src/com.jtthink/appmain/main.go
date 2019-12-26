e70dc4632ce7ca285a6fc1dfede7ba70cba2c990
shijun <shijun@qq.com>
2019-12-26 22:51:31 +0800

    利用阻塞，交替执行协程里面的内容

--- src/com.jtthink/appmain/main.go
+++ src/com.jtthink/appmain/main.go
@@ -13,28 +13,27 @@ func main()  {
 	users:=strings.Split("zhangsan,lisi,wangwu",",")
 	ages:=strings.Split("19,21,22",",")
 
-	c1:=make(chan string,len(users))
-	c2:=make(chan string,len(ages))
+	c1,c2:=make(chan bool),make(chan bool)
+	ret:=make([]string,0)
 
 	go func() {
 		for _,v:=range users{
-			c1<-v
+			<-c1            //读取channel是空，阻塞
+			ret=append(ret, v)
+			c2<-true        //c2设置值，让下面不阻塞
 		}
-		close(c1)
 	}()
 	go func() {
 		for _,v:=range ages{
-			c2<-v
+			<-c2            //c2有值，不阻塞
+			ret=append(ret, v)
+			c1<-true        //c1设置值，让c1不阻塞
 		}
-		close(c2)
 	}()
 
-	for v:=range c1{ //range channel 只有v 没有index。channel可以使用range遍历，但是有区别在于它会一直遍历，直到channel被关闭，(关闭的channel，取值不会阻塞，返回零值)
-		fmt.Println(v)
-	}
-	for v:=range c2{
-		fmt.Println(v)
-	}
+	c1<-true  //主进程运行到这里，设置c1,让c1协程不阻塞。
+	fmt.Println(ret)
+	//这样运行让c1和c2循环阻塞，可能交替打印出c1和c2内容。但是c1和c2里面运行时间长的话，主函数运行打印程序时，协程还未运行结束。此时仅能打印出部分数据
 
 
 
