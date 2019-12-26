40c1b8d770a597e5badb5c3716ffa34ad261549e
shijun <shijun@qq.com>
2019-12-26 21:48:21 +0800

    遍历数组

--- src/com.jtthink/appmain/main.go
+++ src/com.jtthink/appmain/main.go
@@ -4,34 +4,30 @@ import (
 	_ "com.jtthink/Servicesb"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"time"
+	"strings"
 )
 
-func sum(min,max int,c chan int)  {
-	res:=0
-	for i:=min;i<=max;i++ {
-		res=res+i
-	}
-	c<-res
-}
+
 func main()  {
 
-	start:=time.Now()
-	num:=100000000
-	c:=make(chan int,2)
+	users:=strings.Split("zhangsan,lisi,wangwu",",")
+	ages:=strings.Split("19,21,22",",")
+
+	alllist:=make([]string,0)
+
+	//alllist=append(alllist, users...) //合并两个数组，到一个新的切片
+	//alllist=append(alllist,ages...)
+	////结果[zhangsan lisi wangwu 19 21 22]
+
+	//for i,v:=range users{ //循环遍历交替插入
+	//	alllist= append(alllist, v)
+	//	alllist= append(alllist, ages[i])
+	//}
+	////结果[zhangsan 19 lisi 21 wangwu 22]
+
+	fmt.Println(alllist)
 
 
-	////sum(0,num,c) 同步执行
-	//go sum(0,num,c) 协程执行，两者基本相同
-	//res:=<-c //数据接收者
-
-	go sum(0,num/2,c) //将数据分成两份，分别放入两个协程运行。总体运行时间减少为一半
-	go sum(num/2+1,num,c)
-
-	res1,res2:=<-c,<-c
-
-	end:=time.Now()
-	fmt.Println(end.Sub(start),res1+res2)
 
 
 
