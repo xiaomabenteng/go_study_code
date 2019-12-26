06282582c8bc7b2d9afb3d6969f9a1a73db1d410
shijun <shijun@qq.com>
2019-12-26 23:22:27 +0800

    使用go 简单爬取网页内容1并保存

--- src/com.jtthink/appmain/main.go
+++ src/com.jtthink/appmain/main.go
@@ -5,17 +5,23 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"io/ioutil"
+	"net/http"
 )
 
 
 func main()  {
 
-	//文件读取操作
-content:="hello abc"
-ioutil.WriteFile("./files/1.txt",[]byte(content),666)
-byt,_:=ioutil.ReadFile("./files/1.txt")
 
-fmt.Println(string(byt))
+url:="https://news.cnblogs.com/n/page/%d/"
+
+for i:=1;i<=3;i++{
+	url:=fmt.Sprintf(url,i) //格式化字符串
+	res,_:=http.Get(url) //http请求
+	cnt,_:=ioutil.ReadAll(res.Body) //获取body
+	ioutil.WriteFile(fmt.Sprintf("./files/%d",i),cnt,666) //输出文件
+}
+
+
 
 
 
