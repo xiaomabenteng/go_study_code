fef2d328dc699fb32ebd12e822480ab7b1910ae2
shijun <shijun@qq.com>
2019-12-29 11:32:15 +0800

    注释

--- src/com.jtthink/appmain/main.go
+++ src/com.jtthink/appmain/main.go
@@ -18,7 +18,7 @@ c:=make(chan map[int][]byte)
 for i:=1;i<=3;i++{
 
 	go func(index int) {
-		url:=fmt.Sprintf(url,index) //格式化字符串
+		url:=fmt.Sprintf(url,index) //格式化字符串，生成每一页页码url
 		res,_:=http.Get(url) //http请求
 		cnt,_:=ioutil.ReadAll(res.Body) //获取body
 		c<-map[int][]byte{index:cnt} //将序号，和网页内容写入channel
@@ -27,7 +27,7 @@ for i:=1;i<=3;i++{
 		}
 	}(i)
 }
-	for getcnt:=range c {
+	for getcnt:=range c { //主线程干的事，不断地range channel 直到channel关闭
 		for i,v:=range getcnt{
 			ioutil.WriteFile(fmt.Sprintf("./files/%d",i),v,666) //输出文件
 		}
