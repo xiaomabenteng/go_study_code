08624d7f944539ff0abf5394e8225824c984b8fa
shijun <shijun@qq.com>
2019-12-29 21:24:40 +0800

    panic 和defer

--- src/com.jtthink/appmain/main.go
+++ src/com.jtthink/appmain/main.go
@@ -4,34 +4,49 @@ import (
 	_ "com.jtthink/Servicesb"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"io/ioutil"
-	"net/http"
 )
 
+func test()  {
+	a:=1
+	defer func() {
+		fmt.Println(a)
+	}()
+	defer func() {
+		a=10
+	}()
+	defer func() {     //需要定义在异常前面，可以定义多个，执行顺序是最后定义的先执行
+		fmt.Println(a)
+	}()
 
-func main()  {
+	panic("my exp") //throw
 
 
-url:="https://news.cnblogs.com/n/page/%d/"
 
-c:=make(chan map[int][]byte)
-for i:=1;i<=3;i++{
-
-	go func(index int) {
-		url:=fmt.Sprintf(url,index) //格式化字符串，生成每一页页码url
-		res,_:=http.Get(url) //http请求
-		cnt,_:=ioutil.ReadAll(res.Body) //获取body
-		c<-map[int][]byte{index:cnt} //将序号，和网页内容写入channel
-		if index==3{
-			close(c)
-		}
-	}(i)
 }
-	for getcnt:=range c { //主线程干的事，不断地range channel 直到channel关闭
-		for i,v:=range getcnt{
-			ioutil.WriteFile(fmt.Sprintf("./files/%d",i),v,666) //输出文件
-		}
-	}
+func main()  {
+	test()
+//
+//
+//url:="https://news.cnblogs.com/n/page/%d/"
+//
+//c:=make(chan map[int][]byte)
+//for i:=1;i<=3;i++{
+//
+//	go func(index int) {
+//		url:=fmt.Sprintf(url,index) //格式化字符串，生成每一页页码url
+//		res,_:=http.Get(url) //http请求
+//		cnt,_:=ioutil.ReadAll(res.Body) //获取body
+//		c<-map[int][]byte{index:cnt} //将序号，和网页内容写入channel
+//		if index==3{
+//			close(c)
+//		}
+//	}(i)
+//}
+//	for getcnt:=range c { //主线程干的事，不断地range channel 直到channel关闭
+//		for i,v:=range getcnt{
+//			ioutil.WriteFile(fmt.Sprintf("./files/%d",i),v,666) //输出文件
+//		}
+//	}
 
 
 
