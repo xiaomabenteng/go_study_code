0f26dd44279098fb632151bf9567e6067433f99a
shijun <shijun@qq.com>
2019-12-29 22:31:49 +0800

    抓取网页模拟异常抛出和捕获

--- src/com.jtthink/appmain/main.go
+++ src/com.jtthink/appmain/main.go
@@ -4,45 +4,47 @@ import (
 	_ "com.jtthink/Servicesb"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"io/ioutil"
+	"net/http"
 )
 
-func test() (ret int) {
-	defer func() {
-		ret=-1
-	}()
+func test() {
 	panic("exp")
 }
 
 func main()  {
-	defer func() {
-		err:=recover()  //recover()用于panic信息捕获
-		if err!=nil{
-			fmt.Println(err)
+
+
+
+url:="https://news.cnblogs.com/n/page/%d/"
+
+c:=make(chan map[int][]byte)
+for i:=1;i<=3;i++{
+
+	go func(index int) {
+		defer func() {
+			if err:=recover();err!=nil{ //捕获异常并处理
+				fmt.Println(err)
+			}
+			if index==3 { //关闭通道，否则主函数一直在执行
+				close(c)
+			}
+		}()
+		url:=fmt.Sprintf(url,index) //格式化字符串，生成每一页页码url
+		res,_:=http.Get(url) //http请求
+		cnt,_:=ioutil.ReadAll(res.Body) //获取body
+		if index==3 {  //模拟在执行第三页网页抓取时抛出异常
+			test()
 		}
-	}()
-	fmt.Println(test())
-//
-//
-//url:="https://news.cnblogs.com/n/page/%d/"
-//
-//c:=make(chan map[int][]byte)
-//for i:=1;i<=3;i++{
-//
-//	go func(index int) {
-//		url:=fmt.Sprintf(url,index) //格式化字符串，生成每一页页码url
-//		res,_:=http.Get(url) //http请求
-//		cnt,_:=ioutil.ReadAll(res.Body) //获取body
-//		c<-map[int][]byte{index:cnt} //将序号，和网页内容写入channel
-//		if index==3{
-//			close(c)
-//		}
-//	}(i)
-//}
-//	for getcnt:=range c { //主线程干的事，不断地range channel 直到channel关闭
-//		for i,v:=range getcnt{
-//			ioutil.WriteFile(fmt.Sprintf("./files/%d",i),v,666) //输出文件
-//		}
-//	}
+		c<-map[int][]byte{index:cnt} //将序号，和网页内容写入channel
+
+	}(i)
+}
+	for getcnt:=range c { //主线程干的事，不断地range channel 直到channel关闭
+		for i,v:=range getcnt{
+			ioutil.WriteFile(fmt.Sprintf("./files/%d",i),v,666) //输出文件
+		}
+	}
 
 
 
