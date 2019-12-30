ac5caf6690c094667dc74892fd6f20dcf1aa77ac
shijun1 <shijun1@xin.com>
2019-12-30 17:19:44 +0800

    io和bufio

--- src/com.jtthink/appmain/main.go
+++ src/com.jtthink/appmain/main.go
@@ -1,40 +1,42 @@
 package main
 
 import (
+	"bufio"
 	_ "com.jtthink/Servicesb"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"sync"
+	"os"
+	"time"
 )
 
 
 func main()  {
 
-	var WG sync.WaitGroup
-	var mutex sync.Mutex
-	WG.Add(2)
-	list:=make([]int,0)  //两个协程并行运行，会出现协程安全性问题，在一个协程还未执行完时，被切换到其他协程，导致数据出错。使用锁机制，解决
-	go func() {
-		defer WG.Done()
-		mutex.Lock()      //设置锁
-		for i:=1;i<=1000000;i++{
-			list=append(list,1)
-		}
-		fmt.Println(len(list))
-		mutex.Unlock() //协程执行完毕释放锁
-	}()
-	go func() {
-		defer WG.Done()
-		mutex.Lock()
-		for i:=1;i<=1000000;i++{
-			list=append(list,1)
-		}
-		fmt.Println(len(list))
-		mutex.Unlock()
-	}()
+	////定义一个变量，并输出到控制台
+	//var myname bytes.Buffer
+	//myname.Write([]byte("zhangsan"))
+	//myname.WriteTo(os.Stdout)
 
-	WG.Wait()
-	fmt.Println(len(list))
+	////写入文件
+	//file,_:=os.OpenFile("./test/test",os.O_RDWR | os.O_CREATE | os.O_APPEND,666 )
+	//defer file.Close()
+	//file.Write([]byte("zhangsan"))
 
+	////每隔三秒写入文件。频繁的对磁盘读写会造成不必要的性能开支和对磁盘影响
+	//file,_:=os.OpenFile("./test/test",os.O_RDWR | os.O_CREATE | os.O_APPEND | os.O_TRUNC,666 )
+	//defer file.Close()
+	//for i:=1;i<10;i++ {
+	//	file.Write([]byte("zhangsan\n"))
+	//	time.Sleep(time.Second*2)
+	//}
+
+	//每隔三秒写入文件。
+	file,_:=os.OpenFile("./test/test",os.O_RDWR | os.O_CREATE | os.O_APPEND | os.O_TRUNC,666 )
+	defer file.Close()
+	fw:=bufio.NewWriter(file) //使用bufio包，自带缓冲区默认4096，当达到一定量才进行下一步操作
+	for i:=1;i<3;i++ {
+		fw.Write([]byte("zhangsan\n"))
+		time.Sleep(time.Second*2)
+	}
+	fw.Flush() //如果一直未达到设置缓冲大小。手动刷新缓冲，把内容写入
 
 }
