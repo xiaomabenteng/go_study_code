c0a77f456b5f03646d9a4b1fdc92a295830d9394
shijun1 <shijun1@xin.com>
2019-12-31 13:58:27 +0800

    路由，设置cookie, 注销demo

--- src/com.jtthink.net/httpserver/main.go
+++ src/com.jtthink.net/httpserver/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 type MyHandler struct {
 
@@ -20,36 +21,32 @@ func (*MyHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request)  
 
 func main()  {
 
-	//http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-	//	writer.Write([]byte("hello"))
-	//})
-	//http.HandleFunc("/abc", func(writer http.ResponseWriter, request *http.Request) {
-	//	writer.Write([]byte("abc"))
-	//})
-	////err:=http.ListenAndServe(":8099",nil) //启动一个http服务，监听指定端口
-	////if err!=nil {
-	////	fmt.Println(err)
-	////}
-	//server:=http.Server{Addr:":8099",Handler:nil} //第二种方式启动http服务，其实和第一种是一样的
-	//err:=server.ListenAndServe()
-	//if err!=nil{
-	//	fmt.Println(err)
-	//}
-
-
-
-	//handler:=new(MyHandler)
-	//server:=http.Server{Addr:":8099",Handler:handler} //传递自定义handler
-	//err:=server.ListenAndServe()
-	//if err!=nil{
-	//	fmt.Println(err)
-	//}
 
 	mymux:=http.NewServeMux()                // 自定义路由管理
-	mymux.Handle("/hello",new(MyHandler)) //第一种方式,使用自定义handler
+	mymux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {  //
+		writer.Write([]byte("index"))
+	})
+	mymux.HandleFunc("/login", func(writer http.ResponseWriter, request *http.Request) {  //
 
-	mymux.HandleFunc("/abc", func(writer http.ResponseWriter, request *http.Request) {  // 第二种方式
-		writer.Write([]byte("abc"))
+		getUserName:=request.URL.Query().Get("username")
+		if getUserName!=""{
+			 c:=&http.Cookie{Name:"username",Value:getUserName}
+			 http.SetCookie(writer,c)
+		}
+		writer.Write([]byte("登录页"))
+	})
+	mymux.HandleFunc("/logout", func(writer http.ResponseWriter, request *http.Request) {  //
+		c:=&http.Cookie{Name:"username",Value:"",Expires:time.Now().AddDate(-1,0,0)} //设置cookie过期
+		http.SetCookie(writer,c)
+
+		writer.Header().Set("Content-type","text/html")
+		writer.Write([]byte("unlogin....."))
+		script:=`<script>
+		setTimeout(()=>{self.location='/'},2000)
+</script>`
+		writer.Write([]byte(script))
+
+
 	})
 	server:=http.Server{Addr:":8099",Handler:mymux}
 	err:=server.ListenAndServe()
